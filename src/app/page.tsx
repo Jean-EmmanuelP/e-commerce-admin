939ed10f018d83a9e571754f09c02a652d6e1e8d
Jean-EmmanuelP <Vous@exemple.com>
2023-05-20 10:44:08 +0200

    doing dashboard done the session name , mail and image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,24 @@
 
 import { useSession, signIn, signOut } from "next-auth/react";
 import Layout from "@/components/Layout.jsx";
+import { notFound } from "next/navigation";
 
 export default function Home() {
-  return <Layout>Test</Layout>;
+  const { data: session } = useSession();
+
+  return (
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>Hello, {session?.user?.email}</h2>
+        <div className="flex bg-gray-300 text-black gap-1">
+          <img
+            src={session?.user?.image as string}
+            alt=""
+            className="w-6 h-6"
+          />
+          {session?.user?.name}
+        </div>
+      </div>
+    </Layout>
+  );
 }
