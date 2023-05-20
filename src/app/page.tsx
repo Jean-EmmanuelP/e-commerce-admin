a191bbb11207465f1b588e370a25e8bf148b8c94
Jean-EmmanuelP <Vous@exemple.com>
2023-05-20 12:28:43 +0200

    connexion with the api/products done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,14 @@ export default function Home() {
   return (
     <Layout>
       <div className="text-blue-900 flex justify-between">
-        <h2>Hello, {session?.user?.email}</h2>
-        <div className="flex bg-gray-300 text-black gap-1">
+        <h2>Hello, <b>{session?.user?.email}</b></h2>
+        <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
           <img
             src={session?.user?.image as string}
             alt=""
             className="w-6 h-6"
           />
-          {session?.user?.name}
+          <span className="px-2">{session?.user?.name}</span>
         </div>
       </div>
     </Layout>
