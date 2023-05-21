22793a613a6a54c14d011e05ca630720d20cddd3
Jean-EmmanuelP <Vous@exemple.com>
2023-05-21 18:49:44 +0200

    doing the upload endpoint for the images req

--- src/components/ProductsForm.tsx
+++ src/components/ProductsForm.tsx
@@ -9,6 +9,7 @@ interface ProductFormProps {
   title?: string | null;
   description?: string | null;
   price?: number | null;
+  images: any;
 }
 
 export default function ProductForm({
@@ -16,6 +17,7 @@ export default function ProductForm({
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
+  images,
 }: ProductFormProps) {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
@@ -38,6 +40,22 @@ export default function ProductForm({
   if (goToProducts) {
     return redirect("/products");
   }
+
+  async function uploadImages(ev: React.ChangeEvent<HTMLInputElement>) {
+    const { files } = ev.target
+
+    if ( files!.length > 0 ) {
+      const fileList = Array.from(files!)
+      const data = new FormData()
+          fileList.forEach(async (file: File) => {
+            data.append('file', file)
+            const res = await axios.post('/api/upload', data)
+            console.log(file)
+            console.log(res.data)
+          })
+    }
+  }
+
   return (
     <form onSubmit={saveProduct}>
       <label>Product name</label>
@@ -47,6 +65,28 @@ export default function ProductForm({
         value={title}
         onChange={(ev) => setTitle(ev.target.value)}
       />
+      <label>Photos</label>
+      <div className="mb-2">
+        <label className="w-24 h-24 border text-center cursor-pointer flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <div>Upload</div>
+          <input type="file"  onChange={uploadImages} className="hidden"/>
+        </label>
+        {!images?.length && <div>No photos in this product</div>}
+      </div>
       <label>Description</label>
       <textarea
         placeholder="description"
