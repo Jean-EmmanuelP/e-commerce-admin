c3b90b4272d79b9d492763e56f78f95aa5463b1f
Jean-EmmanuelP <Vous@exemple.com>
2023-05-22 08:57:53 +0200

    connection with the s3 aws done

--- src/pages/api/upload.js
+++ src/pages/api/upload.js
@@ -1,4 +1,9 @@
 import multiparty from "multiparty";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import fs from "fs";
+import mime from "mime-types";
+
+const bucketName = "jean-emmanuel-next-ecommerce";
 
 export default async function handle(req, res) {
   const form = new multiparty.Form();
@@ -15,7 +20,33 @@ export default async function handle(req, res) {
     });
 
     console.log("length: ", files.file.length);
-    res.json("ok");
+    const client = new S3Client({
+      region: "us-east-1",
+      credentials: {
+        accessKeyId: process.env.S3_ACCESS_KEY,
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+      },
+    });
+
+    const links = []
+    for (const file of files.file) {
+      const ext = file.originalFilename.split(".").pop();
+      const newFilename = Date.now() + "." + ext;
+
+      console.log({ ext, file });
+      await client.send(
+        new PutObjectCommand({
+          Bucket: bucketName,
+          Key: newFilename,
+          Body: fs.readFileSync(file.path),
+          ACL: "public-read",
+          ContentType: mime.lookup(file.path),
+        })
+      );
+      const link = `https://${bucketName}.s3.amazonaws.com/${newFilename}`;
+      links.push(link)
+    }
+    res.json({links});
   } catch (error) {
     console.error("An error occurred during form parsing", error);
     res.status(500).json("Internal Server Error");
