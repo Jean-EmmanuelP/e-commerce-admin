b810f03b5a5e6132a0f3fe5ca121c6a3ae594b1e
Jean-EmmanuelP <Vous@exemple.com>
2023-06-11 16:23:24 +0200

    added the localDate for the Orders

---
+++ src/app/(orders)/orders/page.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import Layout from "@/components/Layout";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function OrderPage() {
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    axios.get("/api/order").then((response) => {
+      setOrders(response.data);
+    });
+  }, []);
+  return (
+    <Layout>
+      <h1>Orders</h1>
+      <table className="basic">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Recipient</th>
+            <th>Products</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.length > 0 &&
+            orders.map((order: any) => (
+              <tr>
+                <td>{(new Date(order.createdAt)).toLocaleString()}</td>
+                <td>
+                  {order.name}
+                  {order.email}
+                  <br />
+                  {order.city}
+                  {order.postalCode}
+                  {order.country}
+                  <br />
+                  {order.streetAddress}
+                  <br />
+                </td>
+                <td>
+                  {order.line_items.map((l: any) => (
+                    <>
+                      {
+                        (l.price_data.productData || l.price_data.product_data)
+                          ?.name
+                      }{" "}
+                      x {l.quantity}
+                      {JSON.stringify(l)}
+                      <br />
+                    </>
+                  ))}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
+}
